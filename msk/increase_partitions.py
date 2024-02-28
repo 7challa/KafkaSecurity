c2d372028f498dd747a944d1a3b93adfb9053576
challa7@outlook.com <shrchall1@WKMZT073DE3B.global.publicisgroupe.net>
2024-02-28 11:30:50 -0600

    updated increase partitions

--- msk/increase_partitions.py
+++ msk/increase_partitions.py
@@ -27,74 +27,56 @@ def get_topics_from_cluster(bootstrap_servers):
     return topics_list
 
 
-def increase_partitions(topics, bootstrap_servers):
+def increase_partitions(bootstrap_servers, topic_name, num_partitions):
     existing_topics_list = get_topics_from_cluster(bootstrap_servers)
+    # Update retention to 5 seconds
+    config_value = "retention.ms=" + num_partitions
 
-    for topic in topics:
-        if topic['name'] not in existing_topics_list:
-            print("Skipping alter topic: {} - Topic doesn't exist".format(topic['name']))
-            continue
+    if topic_name not in existing_topics_list:
+        print("Topic: {} - doesn't exist".format(topic_name))
+        sys.exit(1)
+    else:
+        desc_command = ["/opt/app/kafka-2.8.1/bin/kafka-topics.sh", "--bootstrap-server", " ", "--describe", "--topic", topic_name, "--command-config", "/opt/app/kafka-2.8.1/client.properties"]
+        desc_command[2] = bootstrap_servers
 
-        # Creating topic if doesn't exist
-        topic_name = topic['name']
-        # print("Topic: {}, type: {}".format(topic_name, type(topic_name)))
-        num_partitions = str(topic['partitions'])
-
-        command = ["/opt/app/kafka-2.8.1/bin/kafka-topics.sh", "--bootstrap-server", " ", "--alter", "--topic", topic_name,
-                   "--partitions", num_partitions, "--command-config", "/opt/app/kafka-2.8.1/client.properties"]
-        command[2] = bootstrap_servers
-        # print(command)
-
-        process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-
+        process = subprocess.Popen(desc_command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         output, error = process.communicate()
 
         if error:
-            print("Unable to alter topic")
+            print("Unable to describe topic on the cluster")
             print(error.decode())
         else:
             print(output.decode())
-            print("Topic is altered!")
+            print()
 
-        command2 = ["/opt/app/kafka-2.8.1/bin/kafka-topics.sh", "--bootstrap-server", " ", "--describe", "--topic",
-                   topic_name, "--command-config", "/opt/app/kafka-2.8.1/client.properties"]
-        command2[2] = bootstrap_servers
-        process = subprocess.Popen(command2, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        alter_command = ["/opt/app/kafka-2.8.1/bin/kafka-topics.sh", "--bootstrap-server", " ", "--alter", "--topic", topic_name,
+                   "--partitions", num_partitions, "--command-config", "/opt/app/kafka-2.8.1/client.properties"]
+        alter_command[2] = bootstrap_servers
 
-        output2, error2 = process.communicate()
+        alter_process = subprocess.Popen(alter_command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        output, error = alter_process.communicate()
 
         if error:
-            print("Unable to describe topic")
-            print(error2.decode())
+            print("Unable to alter topic configuration on topic {}".format(topic_name))
+            print(error.decode())
         else:
-            print(output2.decode())
-
-
-
-def load_topics_from_yaml(yaml_file):
-    with open(yaml_file, 'r') as file:
-        topics = yaml.safe_load(file)
-    return topics['topics']
+            print(output.decode())
 
 def check_arguments():
     arguments_length = len(sys.argv) - 1
-    if arguments_length > 2 or arguments_length < 2 or arguments_length == 0:
+    if arguments_length != 3:
         print("Incorrect arguments passed. Exiting...")
-        print("Usage: increase_partitions.py <bootstrap-servers> <alter_topics.yaml>")
-        print('Example: increase_partitions.py "localhost:9098" alter_topics.yaml')
+        print("Usage: increase_partitions.py <bootstrap-servers> <topic_name>")
+        print('Example: increase_partitions.py "localhost:9098" alter_snaplogic_feed 5')
         sys.exit(1)
 
 if __name__ == "__main__":
     check_arguments()
     bootstrap_servers = sys.argv[1]
-    yaml_file = sys.argv[2]
+    topic_name = sys.argv[2]
+    num_partitions = sys.argv[3]
 
     try:
-        topics = load_topics_from_yaml(yaml_file)
+        increase_partitions(bootstrap_servers,topic_name, num_partitions)
     except Exception as e:
-        print("Error loading yaml file. {}".format(e))
-
-    try:
-       increase_partitions(topics, bootstrap_servers)
-    except Exception as e:
-       print("Error altering topic! {}".format(e))
+        print("Error updating topic partitions. {}".format(e))
